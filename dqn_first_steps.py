ea6b64dbd4bef7bf2235eff4e99b1337aa95b67f
Ubuntu <ubuntu@ip-172-31-5-111.ec2.internal>
2016-08-19 04:33:05 +0000

    removed.swp files, added model to dqn_first_steps

--- dqn_first_steps.py
+++ dqn_first_steps.py
@@ -1,3 +1,31 @@
 import first_steps
+import numpy as np
 
-env = first_steps.env()
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten
+from keras.optimizers import Adam
+
+from rl.agents.dqn import DQNAgent
+from rl.policy import BoltzmannQPolicy
+from rl.memory import SequentialMemory
+
+env = first_steps.Env()
+nb_actions = env.action_space.n
+
+model = Sequential()
+model.add(Flatten(input_shape=(1,1)))
+model.add(Dense(16))
+model.add(Activation('relu'))
+model.add(Dense(16))
+model.add(Activation('relu'))
+model.add(Dense(16))
+model.add(Activation('relu'))
+model.add(Dense(nb_actions))
+model.add(Activation('linear'))
+print(model.summary())
+
+memory = SequentialMemory(limit=50000)
+policy = BoltzmannQPolicy()
+dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=10,
+                       target_model_update=1e-2, policy=policy)
+dqn.compile(Adam(lr=1e-3), metrics=['mae'])
